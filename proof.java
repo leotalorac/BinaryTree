af69e1652c655beed8e029a046d9784c32658fa8
lotalorafox <lotalorafox@outlook.com>
2018-04-24 09:33:00 -0500

    random trees

--- proof.java
+++ proof.java
@@ -1,15 +1,33 @@
 /**
  * proof
+ * by lotaorafox
  */
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 public class proof {
     public static void main(String[] args) {
+        Random rand = new Random();
+        ArrayList<Integer> nums = new ArrayList<>();
+        System.out.print("Generating a random tree .........");
         Scanner sc = new Scanner(System.in);
         BinaryTree aleja = new BinaryTree();
-        int n = sc.nextInt();
+        int n = rand.nextInt(20) + 10;
+        System.out.print("size: " + n );
+        System.out.println();
+        System.out.println("Tree: ");
         for(int i=0;i<n;i++){
-            aleja.insert(sc.nextInt());
+            int num = rand.nextInt(150) + 1;
+            if(nums.contains(num)){
+                i--;
+            }else{
+                nums.add(num);
+                System.out.print(num + " ");
+                aleja.insert(num);
+            }
+            
         }
+        System.out.println();
         System.out.println("PostOrder");
         aleja.printPostOrder(aleja.root);
         System.out.println();
